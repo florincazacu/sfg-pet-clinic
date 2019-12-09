b8319fcbe299e5a2dd51643787415f6f01fe464c
Florin Cazacu <florin.cazacu@radcom.com>
2019-12-09 11:23:05 +0200

    Add Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,37 @@
+package com.example.sfgpetclinic.services.map;
+
+import com.example.sfgpetclinic.model.Specialty;
+import com.example.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+	
+	@Override
+	public Set<Specialty> findAll() {
+		return super.findAll();
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+	
+	@Override
+	public void delete(Specialty entity) {
+		super.delete(entity);
+	}
+	
+	@Override
+	public Specialty save(Specialty entity) {
+		return super.save(entity);
+	}
+	
+	@Override
+	public Specialty findById(Long id) {
+		return super.findById(id);
+	}
+	
+}
